1cd4fa82c46c59277d1c487c57b95c2e47f389d4
tenorok <mail@tenorok.ru>
2013-12-11 23:22:48 +0400

    Правильный тест метода получения всех модулей для сборки

--- test/maker/getmodules.js
+++ test/maker/getmodules.js
@@ -7,7 +7,26 @@ describe('getModules', function() {
     it('getModules modules', function(done) {
         new Maker().getModules(path.join(__dirname, 'modules')).then(function(modules) {
 
-            assert.deepEqual(Object.keys(modules), ['a', 'b', 'c', 'd', 'e', 'f']);
+            var standardModules = {
+                    'a': [],
+                    'b': ['a'],
+                    'c': ['a', 'b'],
+                    'd': ['a', 'b', 'c'],
+                    'e': ['d'],
+                    'f': []
+                },
+                resultModules = Object.keys(modules);
+
+            // Проверка наличия модуля в результирующем объекте
+            // Из-за асинхронности порядок полученных модулей не гарантируется
+            Object.keys(standardModules).forEach(function(module) {
+                assert.isTrue(!!~resultModules.indexOf(module));
+            });
+
+            // Проверка правильного получения зависимостей каждого модуля
+            for(var module in modules) {
+                assert.deepEqual(modules[module].dependencies, standardModules[module]);
+            }
 
             done();
         }).done();
