d59ecd392b6676be44eb5622be912a7e4a25f305
tenorok <mail@tenorok.ru>
2014-01-06 21:11:51 +0400

    Обновлены тесты на новое имя глобальной функции

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 var assert = require('chai').assert,
-    define = require('../definer.js').define;
+    definer = require('../definer.js').definer;
 
 describe('Простой тест работы модулей.', function() {
 
     it('Модуль a', function() {
-        var a = define('a', function() { return 'a'; });
+        var a = definer('a', function() { return 'a'; });
         assert.equal(a, 'a');
     });
 
     it('Модуль b', function() {
-        var b = define('b', function(a) { return a + 'b'; });
+        var b = definer('b', function(a) { return a + 'b'; });
         assert.equal(b, 'ab');
     });
 
     it('Модуль c', function() {
-        var c = define('c', function(a, b) { return a + b + 'c'; });
+        var c = definer('c', function(a, b) { return a + b + 'c'; });
         assert.equal(c, 'aabc');
     });
 
     it('Модуль d', function() {
-        var d = define('d', function(c) { return c + '!'; });
+        var d = definer('d', function(c) { return c + '!'; });
         assert.equal(d, 'aabc!');
     });
 
