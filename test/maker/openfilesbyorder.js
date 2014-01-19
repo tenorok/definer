5ecb762d3a7572db425ac138972dc44883296120
tenorok <mail@tenorok.ru>
2014-01-19 22:14:02 +0400

    Тесты метода открытия списка файлов по порядку

---
+++ test/maker/openfilesbyorder.js
@@ -0,0 +1,57 @@
+var path = require('path'),
+    assert = require('chai').assert,
+    Maker = require('../../maker.js');
+
+describe('Тестирование метода openFilesByOrder.', function() {
+
+    var directory = path.join(__dirname, 'clean/libs');
+
+    function getFullPath(file) {
+        return path.join(directory, file);
+    }
+
+    function getFileList() {
+        return [
+            getFullPath('jquery.js'),
+            getFullPath('jquery.ui.js'),
+            getFullPath('plugins/plugin.jquery.js'),
+            getFullPath('underscore.js')
+        ];
+    }
+
+    it('Проверка результирующего массива содержимого файлов', function(done) {
+
+        var standardResult = "var $ = function() { return 'jquery'; };" +
+            "$.ui = function() { return 'jquery.ui'; };" +
+            "$.plugin = function() { return 'jquery.plugin'; };" +
+            "var _ = function() { return 'underscore'; };";
+
+        new Maker({
+            directory: directory,
+            verbose: ['error']
+        })
+            .openFilesByOrder(getFileList())
+            .then(function(content) {
+                assert.equal(content.join(''), standardResult);
+                done();
+            }).done();
+    });
+
+    it('Проверка работы колбека', function(done) {
+
+        var filesData = {};
+
+        new Maker({
+            directory: directory,
+            verbose: ['error']
+        })
+            .openFilesByOrder(getFileList(), function(file, data) {
+                filesData[file] = data;
+            })
+            .then(function() {
+                assert.deepEqual(Object.keys(filesData), getFileList());
+                done();
+            }).done();
+    });
+
+});
