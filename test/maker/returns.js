b1464098863cbf40e9a4ca59fbb26356b7666a98
tenorok <mail@tenorok.ru>
2014-05-14 16:11:29 +0400

    Тест функции экспорта

---
+++ test/maker/returns.js
@@ -0,0 +1,36 @@
+var path = require('path'),
+    fs = require('fs'),
+    vow = require('vow'),
+    assert = require('chai').assert,
+    Maker = require('../../maker.js'),
+    helper = require('./helper.js');
+
+describe('Тестирование функции экспорта.', function() {
+
+    var savePromise = vow.promise(),
+        saveFilePath = path.join(__dirname, 'modules/all.js');
+
+    it('Сборка', function(done) {
+
+        new Maker({
+            directory: path.join(__dirname, 'modules2'),
+            module: 'z',
+            verbose: ['error']
+        }).make(saveFilePath).then(function() {
+
+                fs.readFile(saveFilePath, { encoding: 'UTF-8' }, function(err, data) {
+                    assert.equal(data, helper.getClosureStringReturnsModuleZ());
+                    savePromise.fulfill();
+                });
+
+                done();
+            }).done();
+    });
+
+    after(function(done) {
+        vow.all([savePromise, helper.unlink(saveFilePath)]).then(function() {
+            done();
+        });
+    });
+
+});
