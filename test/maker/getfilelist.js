8a6178398e31669ab71109dc2bbc1a128d7798e1
tenorok <mail@tenorok.ru>
2014-05-15 15:37:51 +0400

    Добавлен тестовый модуль w

--- test/maker/getfilelist.js
+++ test/maker/getfilelist.js
@@ -45,6 +45,7 @@ describe('Тестирование метода getFileList.', function() {
                 'modules/sub/sub/a.js',
                 'modules/sub/sub/ef.js',
                 'modules/sub/sub/fake.js',
+                'modules2/w.js',
                 'modules2/x.js',
                 'modules2/y.js',
                 'modules2/z.js'
